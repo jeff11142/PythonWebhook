3b3bbf917e221e933f4ca10cc7898087039135c9
jeff11142 <doggy90739@gmail.com>
2024-01-08 17:23:28 +0800

    update algo_webhook

--- algo.py
+++ algo.py
@@ -3,4 +3,4 @@ from flask import jsonify, request
 def algo_webhook(request):
     data = request.json
 
-    return jsonify({"status": "success", "message": "Webhook received and processed"})
+    return jsonify({"status": "success", "message": request.json})
