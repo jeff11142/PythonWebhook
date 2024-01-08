9b5684ee191dc1a6d57ec611f5692388ff81acad
jeff11142 <doggy90739@gmail.com>
2024-01-08 20:12:59 +0800

    fix algo print

--- algo.py
+++ algo.py
@@ -2,5 +2,5 @@ from flask import jsonify, request
 
 def algo_webhook(request):
     data = request.json
-
+    print("algo data = "+data)
     return jsonify({"status": "success", "message": request.json})
